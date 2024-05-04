a062e139db7e72806fc3670c3164621dd8f08cb0
alejandroP03 <d.pulidob@uniandes.edu.co>
2024-05-04 09:50:26 -0500

    env var added

--- offers_notify_service/init.py
+++ offers_notify_service/init.py
@@ -1,9 +1,10 @@
-import pika
+import os
 
+import pika
 from notifier import make_sms_notification
 
 if __name__ == '__main__':
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST')))
     channel = connection.channel()
 
     channel.queue_declare(queue='notify_service_queue', durable=True)
